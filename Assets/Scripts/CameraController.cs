9b38758f7cc76086f75e61564191391edb8fa955
Sam Johnson <samwj02@gmail.com>
2018-11-27 11:56:05 +0000

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+
+    public bool UseOffsetValues;
+
+    public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+        if(!UseOffsetValues)
+        offset = target.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Get the x position of the mouse & rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        //Move the camera based on the current rotation of the target and the original offset
+        float desiredYAngle = target.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+
+        transform.LookAt(target);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 18;
+    //public Vector3 jump;
+    public float jumpForce = 2.0f;
+    //public bool isGrounded;
+    public CharacterController controller;
+    //https://youtu.be/IeuqDVKfJag?list=PLiyfvmtjWC_V_H-VMGGAZi7n5E0gyhc37&t=543
+    //Rigidbody rb;
+    public float gravityScale;
+
+    private Vector3 moveDirection;
+    //private Rigidbody rig;
+
+    void Start () {
+        //rig = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+	}
+
+    void OnCollisionStay()
+    {
+        //isGrounded = true;
+    }
+
+    void OnCollisionExit()
+    {
+        //isGrounded = false;
+    }
+
+    void Update()
+    {
+        /*
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+        rig.MovePosition(transform.position + movement);
+        */
+
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
